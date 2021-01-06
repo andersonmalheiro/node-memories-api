746d5fa9137794032de9c487abfec6013c9691ad
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-01-05 21:56:12 -0300

    Updating user entitity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,16 +1,12 @@
-import { uuid } from "uuidv4";
-
 export class User {
   public readonly id!: string;
   public name!: string;
   public email!: string;
   public password!: string;
+  public created_at!: string;
+  public updated_at!: string;
 
-  constructor(props: Omit<User, "id">, id?: string) {
+  constructor(props: Omit<User, "id" | "created_at" | "updated_at">) {
     Object.assign(this, props);
-
-    if (!id) {
-      this.id = uuid();
-    }
   }
 }
